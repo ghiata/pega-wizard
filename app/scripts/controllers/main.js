8ed6f226a542663ed1f8603f72b29dc8d678791f
Folkert Schram de Jong <folkert.schramdejong@capgemini.com>
2014-05-16 17:45:04 +0200

    Add user model

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('pegaWizardApp', ['mgo-angular-wizard'])
-    .controller('WizardCtrl', function($scope, WizardHandler) {
+    .controller('WizardCtrl', function($scope, $http, WizardHandler) {
         $scope.finished = function() {
             alert("Wizard finished :)");
         }
@@ -13,4 +13,34 @@ angular.module('pegaWizardApp', ['mgo-angular-wizard'])
         $scope.goBack = function() {
             WizardHandler.wizard().goTo(0);
         }
+
+        $scope.getOptions = function() {
+            alert($scope.user);
+            // Get the loans options from the API
+            $http({method: 'POST', url: '/someUrl'}).
+                success(function(data, status, headers, config) {
+                  // this callback will be called asynchronously
+                  // when the response is available
+                }).
+                error(function(data, status, headers, config) {
+                  // called asynchronously if an error occurs
+                  // or server returns response with an error status.
+                });
+        }
+
+        $scope.master = {};
+
+        $scope.update = function(user) {
+            $scope.master = angular.copy(user);
+        };
+
+        $scope.reset = function() {
+            $scope.user = angular.copy($scope.master);
+        };
+
+        $scope.isUnchanged = function(user) {
+            return angular.equals(user, $scope.master);
+        };
+
+        $scope.reset();
     });
